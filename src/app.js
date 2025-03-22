3651eb13882ed58890f7e9723532e584108b8270
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-03-22 02:05:19 +0200

    feat: add Swagger UI for API documentation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+
 require('dotenv').config();
+
+const swaggerDefinition = require('./docs/swagger.json');
 const authRoutes = require('./routes/authRoutes');
 const todoRoutes = require('./routes/todoRoutes');
 
@@ -15,6 +19,9 @@ app.use(cors({
 
 app.use(express.json());
 
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
+
 const mongoUri = process.env.MONGO_URI;
 
 mongoose.connect(mongoUri)
@@ -26,4 +33,5 @@ app.use('/api/todo', todoRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log(`API documentation available at http://localhost:${port}/api-docs`);
 });
